0dcd610498649369c3b9df3f7a2b5a97d5be5748
ww386362087 <tongwuwei@163.com>
2016-10-19 13:34:59 +0800

    地图
    地图相关

--- Chain/Assets/Scripts/Test/View/Terrain.cs
+++ Chain/Assets/Scripts/Test/View/Terrain.cs
@@ -2,14 +2,71 @@
 using System.Collections;
 
 public class Terrain : MonoBehaviour {
+    private Transform player;
+    // ID
+    private int id;
+    // 地图类型
+    private MapType mapType;
+    // 地图名字
+    private string name;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public MapType MapType
+    {
+        get
+        {
+            return mapType;
+        }
+
+        set
+        {
+            mapType = value;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+
+        set
+        {
+            name = value;
+        }
+    }
+
+
+    void Start()
+    {
+        GameObject playerGo = GameObject.FindGameObjectWithTag("Player");
+        if (playerGo != null)
+        {
+            player = playerGo.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x > this.gameObject.transform.position.x + 250)
+        {
+            Destroy(this.gameObject);
+            //  Camera.main.SendMessage("UpdateMap", SendMessageOptions.DontRequireReceiver);
+            Camera.main.SendMessage("UpdateMap");
+        }
+    }
 }
